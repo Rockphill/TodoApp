00b0f5632e2f25d57ece11248aec383526f85688
Andrei Sergienko <macwin33@gmail.com>
2022-04-26 20:32:24 +0300

    Add project files.

---
+++ TodoApp/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using TodoApp.Models;
+
+namespace TodoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<TodoModel> _todoDataList;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Windows_Loaded(object sender, RoutedEventArgs e)
+        {
+            _todoDataList= new BindingList<TodoModel>()
+            {
+                new TodoModel(){Text = "test"},
+                new TodoModel(){Text = "dfsdf"}
+            };
+
+            dgTodoList.ItemsSource = _todoDataList;
+            _todoDataList.ListChanged += _todoDataList_ListChanged;      
+        }
+
+        private void _todoDataList_ListChanged(object? sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType==ListChangedType.ItemDeleted || e.ListChangedType==ListChangedType.ItemDeleted)
+            {
+
+            }
+        }
+    }
+}
